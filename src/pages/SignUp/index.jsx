40e6b2f4a4ef2223b81d92a6eea9f5e38b9cd85e
Johnny Deymisson <jdeymisson@gmail.com>
2022-06-14 11:43:46 -0300

    finalizando pagina de cadastro

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,29 +1,31 @@
-import { FiMail, FiLock } from 'react-icons/fi';
+import { FiUser, FiMail, FiLock } from 'react-icons/fi';
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button';
 import { Container, Form, Background } from "./styles";
 
-export function SignIn() {
+export function SignUp() {
 
   return (
     <Container>
+      <Background />
+      
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
-        <h2>Faça seu login</h2>
+        <h2>Crie sua conta</h2>
 
+        <Input placeholder="Nome" type="text" icon={FiUser} />
+        
         <Input placeholder="E-mail" type="email" icon={FiMail} />
 
         <Input placeholder="Senha" type="password" icon={FiLock} />
 
-        <Button title="Entrar" />
+        <Button title="Cadastrar" />
 
-        <a href="#">Criar Conta</a>
+        <a href="#">Volte para o login</a>
       </Form>
-
-      <Background />
     </Container>
   );
 };
